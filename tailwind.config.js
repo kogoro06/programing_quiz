17ebbb6fe1e6c0330d33fd0481d80b6fe551e845
mana <m.komori6517@gmail.com>
2024-12-02 14:17:02 +0900

    feat: daisyUIのカラーテーマ、フォントの設定を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,28 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
-}
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        frankfurter: ['"OPTIFrankfurter-Medium"', 'sans-serif'],
+        mplus: ['"M PLUS Rounded 1c"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    darkTheme: false, // ダークテーマを無効化
+    themes: [
+      {
+        mytheme: {
+          'primary': '#1E3A8A',
+          'secondary': '#F3F4F6',
+          'accent': '#10B981',
+          'font': '#111827',
+          'base-100': '#FFFFFF', // 必須プロパティ (ベース背景カラー)
+        },
+      },
+    ],
+  },
+};
