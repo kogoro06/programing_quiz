6bbcdccee9a9f8247ff3682ec7e2c434d76302da
abusumi <hpbd12201204@gmail.com>
2024-12-03 06:42:15 +0900

    fix:error_massage

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   enum role: { general: 0, admin: 1 } # 役割の定義
 
-  #validates :name, presence: true, length: { in: 1..20 }
-  #validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
-  #validates :password, presence: true, length: { minimum: 6 }
+  validates :name, presence: true, length: { in: 1..20 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
