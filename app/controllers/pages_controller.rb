bc8d1ab1b8b3738470d8d6868a6c3861501fe94d
sora2525 <sky0907a.a@gmail.com>
2024-12-04 16:27:56 +0900

    feat:フッターリンクから遷移可能に→render先の指定

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def contact
+    render "contacts/new"
   end
 
   def terms
+    render "terms_of_services/index"
   end
 
   def privacy
+    render "privacy_policies/index"
   end
 end
