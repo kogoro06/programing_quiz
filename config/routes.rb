2265e3a6e0d5d83907287360fa9a5c19404889fb
sora2525 <sky0907a.a@gmail.com>
2024-12-06 00:39:21 +0900

    feat/indexルーティングを修正、フッターのリンクを適用

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   resource :mypage, only: [ :show, :edit, :update ]
   resources :otherspage, only: [ :show ]
   resources :questions, only: [ :show ]
-  get "questions" => "questions#result", as: :questions
+  get "questions/result"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
