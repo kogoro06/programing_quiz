5361b9cd402182b8814f235c2982604c175f4e5b
taka4655 <taku.enginner@gmail.com>
2024-12-06 17:48:18 +0900

    fix:ビューのフォームの枠だけ用意。フォームからパラメータを受け渡し成功。

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,26 @@
 class ContactsController < ApplicationController
   def new
+    # ログインユーザーかどうかによって、表示を分ける。
+    # deviseの使い方を調べる
+
+    # ゲストユーザーの場合
+    @contact = Contact.new
+    
+    # ログインユーザーの場合
+      # ユーザー情報（ユーザー名とメールアドレス）をインスタンス変数に入れる
+    #@contact = Contact.new
+
   end
 
   def create
+    # これでアクセスできる
+    contact_params
     
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :title, :text)
   end
 end
