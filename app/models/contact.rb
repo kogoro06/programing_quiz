6ab2e5e1f92bd13421cfb9b1684187217339d84d
taka4655 <taku.enginner@gmail.com>
2024-12-06 13:42:57 +0900

    fix:Contactモデルにカラム追加、バリデーション設定

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+  validates :email, presence: true
+  validates :name, presence: true, length: {maximum: 100}
+  validates :title, presence: true, length: {maximum: 100}
+  validates :text, presence: true, length: {maximum: 2000}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_05_103351) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_06_042810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "contacts", force: :cascade do |t|
-    t.string "title"
-    t.string "text"
+    t.string "title", null: false
+    t.string "text", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "email", null: false
   end
 
   create_table "users", force: :cascade do |t|
