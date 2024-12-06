d3da1555c08024b31c39e2f1154139cd06eada0d
sora2525 <sky0907a.a@gmail.com>
2024-12-06 21:13:24 +0900

    feat:ログイン状態によるヘッダーの変化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :set_user
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
+
+  def set_user
+    @user = current_user
+  end
+  
   protected
 
   def configure_permitted_parameters
