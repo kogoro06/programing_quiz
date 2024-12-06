07cf33b55315ce284d6d9570cba25e590e979711
taka4655 <taku.enginner@gmail.com>
2024-12-06 23:18:59 +0900

    add:本番環境用設定追加、メールアドレス・パスワードを環境変数に設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -84,8 +84,8 @@ Rails.application.configure do
    address:              'smtp.gmail.com',
    port:                  587,
    domain:               'gmail.com',
-   user_name:            'questionprograming@gmail.com',
-   password:             'wljm lcau nxtg lnqf',
+   user_name:            ENV["GMAIL_USERNAME"],
+   password:             ENV["GMAIL_PASSWORD"],
    authentication:       'plain',
    enable_starttls_auto:  true
   }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,22 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+
+  config.action_mailer.default_url_options = { host: 'https://programing-quiz-1.onrender.com', protocol: 'https' }
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "https://programing-quiz-1.onrender.com",
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.perform_caching = false
 end
