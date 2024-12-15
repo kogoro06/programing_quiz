ab1ba335399b0be3c42ff684600b33d2413c60b0
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-15 17:30:31 +0900

    fix: エラーメッセージがダブってしまう件を解消 （Userモデルからはpasswordのバリデーション削除）

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,4 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { in: 1..20 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
 end
