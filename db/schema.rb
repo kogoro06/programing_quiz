114cea1a5e4e2646588e97d39bec3ddbf0517a71
abusumi <hpbd12201204@gmail.com>
2024-12-23 04:05:17 +0900

    add:questions_count_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_19_070053) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_22_185841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_19_070053) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.integer "questions_count"
     t.index ["author_user_id"], name: "index_quizzes_on_author_user_id"
   end
 
