601964d2ea697fcf27fae6f98a55bd995b9e340d
abusumi <hpbd12201204@gmail.com>
2024-12-23 21:44:58 +0900

    fix:questions_count_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_22_185841) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_23_123444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,7 +80,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_22_185841) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
-    t.integer "questions_count"
+    t.integer "questions_count", null: false
     t.index ["author_user_id"], name: "index_quizzes_on_author_user_id"
   end
 
