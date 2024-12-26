c966d5c94c45cac6f7c979f290c8c72275102281
taka4655 <taku.enginner@gmail.com>
2024-12-26 11:05:28 +0900

    fix:mypageの動的表示

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -8,21 +8,11 @@ class MypagesController < ApplicationController
                    .select('DATE(quizzes.created_at) as created_date,
                             quizzes.title,
                             quizzes.author_user_id,
+                            quizzes.questions_count as questions_count,
                             users.name as author_name')
                    .where(author_user_id: current_user[:id])
                    .page(params[:page])
-                   .per(2)
-    
-    # TODO: （たかのタスク）タグ機能実装後、タグの表示を実装する
-    # TODO: （たかのタスク）すーさんに問題数をテーブルに保存してもらえるように依頼する。OKであれば、quizテーブルから問題数を取得する
-    # TODO: （たかのタスク）ページネーションの実装
-    # TODO: （たかのタスク）ビューにもTODOを残せるように実装する
-    # TODO: （たかのタスク）必要なファイルだけコミットするようにする
-
-    respond_to do |format|
-      format.html
-      format.js
-    end
+                   .per(6)
   end
 
   def edit
