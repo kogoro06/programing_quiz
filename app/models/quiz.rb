5ebe4f9828dde541f0466c0242d52ae20c346ce1
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-27 15:13:39 +0900

    add: TagとQuizのアソシエーション定義

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,6 @@
 class Quiz < ApplicationRecord
   belongs_to :user, foreign_key: :author_user_id
   has_many :questions
+  has_many :tag_quizzes, dependent: :destroy
+  has_many :tags, through: :tag_quizzes
 end
