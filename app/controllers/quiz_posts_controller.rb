e4d0da9839318333bb24a0e0d52ced6139964f53
abusumi <hpbd12201204@gmail.com>
2025-01-05 19:43:59 +0900

    add:バリデーション・エラーメッセージの追加

--- app/controllers/quiz_posts_controller.rb
+++ app/controllers/quiz_posts_controller.rb
@@ -3,22 +3,39 @@ class QuizPostsController < ApplicationController
 
   def new
     @quiz = Quiz.new
-    # 空のquestionsとchoicesをビルドしてフォームで扱えるようにする
+    if current_user.admin?
+      30.times do
+        question = @quiz.questions.build
+        question.choices.build # 1回だけchoicesを作成
+      end
+    else
       10.times do
-      question = @quiz.questions.build
-      question.choices.build # 1回だけchoicesを作成
+        question = @quiz.questions.build
+        question.choices.build # 1回だけchoicesを作成
+      end
     end
   end
 
   def create
     @quiz = Quiz.new(quiz_params.merge(author_user_id: current_user.id))
+
+    @quiz.questions = @quiz.questions.select { |q| q.question.present? }
+
     if @quiz.save
       redirect_to quiz_path(@quiz), notice: 'クイズを投稿しました！'
     else
-      flash.now[:alert] = 'クイズの投稿に失敗しました。'
-      @page = (params[:page] || 1).to_i
-      @current_question = @quiz.questions[@page - 1]
-      render :new
+      if current_user.admin?
+        30.times do
+          question = @quiz.questions.build
+          question.choices.build # 1回だけchoicesを作成
+        end
+      else
+        10.times do
+          question = @quiz.questions.build
+          question.choices.build # 1回だけchoicesを作成
+        end
+      end
+      render :new, status: :unprocessable_entity
     end
   end
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -6,11 +6,27 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :choices, allow_destroy: true
 
   # 入力があればバリデーションを実行
-  validates :question, presence: true, unless: :optional_question?
-  validates :correct_answer, presence: true, unless: :optional_question?
+  validates :question, presence: true, if: :required_question?
+  validate :validate_correct_answer_and_choices
+  
 
-  # 必須でない質問をスキップ
-  def optional_question?
-    question.blank? && correct_answer.blank?
+
+  def required_question?
+    quiz&.questions&.first == self
+  end
+
+  # questionが入力されている場合に関連バリデーションを有効化
+  def validate_correct_answer_and_choices
+    if question.present?
+      # correct_answerが入力されていない場合のエラーメッセージ
+      if correct_answer.blank?
+        errors.add(:correct_answer, "正解を入力してください")
+      end
+
+      # choicesがすべて入力されていない場合のエラーメッセージ
+      if choices.blank? || choices.any? { |choice| choice.choice1.blank? || choice.choice2.blank? || choice.choice3.blank? || choice.choice4.blank? }
+        errors.add(:choices, "選択肢をすべて入力してください")
+      end
+    end
   end
 end
