c0a0cf3d1eafe0824c8b5c4a2aa4157df8aee1b0
taka4655 <taku.enginner@gmail.com>
2025-01-06 14:27:00 +0900

    lintチェックエラー対応

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ApplicationMailer
     @contact_notification = contact
     mail(
       from: Rails.application.credentials.application_email,
-      to:   Rails.application.credentials.application_email, 
+      to:   Rails.application.credentials.application_email,
       subject: "お問い合わせ通知",
     )
   end
