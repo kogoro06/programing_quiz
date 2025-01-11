4f1f296fb066c7eb5d7a5a30c8ce08969262c4ea
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-01-11 17:28:03 +0900

    fix/プロフィールページの修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,26 +2,16 @@ class ProfilesController < ApplicationController
   before_action :set_user_profile_form, only: [ :edit, :update ]
 
   def show
-    begin
-      @user    = User.find(current_user[:id])
-      @profile = Profile.find_by!(user_id: current_user[:id])
-      # 作成日時を日付に変換
-      @quizzes = Quiz.joins(:user)
-                    .select('DATE(quizzes.created_at) as created_date,
-                              quizzes.title,
-                              quizzes.author_user_id,
-                              quizzes.questions_count as questions_count,
-                              users.name as author_name')
-                    .where(author_user_id: current_user[:id])
-                    .page(params[:page])
-                    .per(6)
-    rescue => e
-      # FIXME: トップページと重複しているので後日リファクタリングする
-      @quizzes = Quiz.eager_load(:user).all
-      @newest_quizzes = @quizzes.order(created_at: :desc).first(6)
-      flash.now[:alert] = "ログインしてください"
-      render template: "quiz_posts/index", status: :unprocessable_entity
-    end
+    @user = current_user
+    @profile = Profile.find_by!(user_id: current_user[:id])
+
+    @quizzes = Quiz.joins(:user) # INNER JOIN で users テーブルを含める
+                   .includes(:tags) # タグを事前ロード
+                   .select('quizzes.*, DATE(quizzes.created_at) as created_date, users.name as author_name')
+                   .where(author_user_id: current_user.id)
+                   .order(created_at: :desc)
+                   .page(params[:page])
+                   .per(6)
   end
 
   def edit
