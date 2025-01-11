324ffbd9ebcca2f4f882ca416ca32ae30e27eabd
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-11 11:18:40 +0900

    fix: 他人のプロフ画面も閲覧できるように値を変更

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,26 +2,21 @@ class ProfilesController < ApplicationController
   before_action :set_user_profile_form, only: [ :edit, :update ]
 
   def show
-    begin
-      @user    = User.find(current_user[:id])
-      @profile = Profile.find_by!(user_id: current_user[:id])
-      # 作成日時を日付に変換
-      @quizzes = Quiz.joins(:user)
-                    .select('DATE(quizzes.created_at) as created_date,
-                              quizzes.title,
-                              quizzes.author_user_id,
-                              quizzes.questions_count as questions_count,
-                              users.name as author_name')
-                    .where(author_user_id: current_user[:id])
-                    .page(params[:page])
-                    .per(6)
-    rescue => e
-      # FIXME: トップページと重複しているので後日リファクタリングする
-      @quizzes = Quiz.eager_load(:user).all
-      @newest_quizzes = @quizzes.order(created_at: :desc).first(6)
-      flash.now[:alert] = "ログインしてください"
-      render template: "quiz_posts/index", status: :unprocessable_entity
-    end
+    @user    = User.find(params[:user_id])
+    @profile = Profile.find_by!(user_id: @user.id)
+    # 作成日時を日付に変換
+    @quizzes = Quiz.joins(:user)
+                  .select('DATE(quizzes.created_at) as created_date,
+                            quizzes.title,
+                            quizzes.author_user_id,
+                            quizzes.questions_count as questions_count,
+                            users.name as author_name')
+                  .where(author_user_id: current_user[:id])
+                  .page(params[:page])
+                  .per(6)
+    # FIXME: トップページと重複しているので後日リファクタリングする
+    @quizzes = Quiz.eager_load(:user).where(author_user_id: @user.id).page(params[:page]).per(6)
+    @newest_quizzes = @quizzes.order(created_at: :desc).first(6)
   end
 
   def edit
