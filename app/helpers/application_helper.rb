13615b318dc2db7e41fe5238725036162a94bb15
taka4655 <taku.enginner@gmail.com>
2025-01-14 21:38:34 +0900

    動的OGP完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
   def prepare_meta_tags(quiz)
-    image_url = "#{request.base_url}/images/ogp.png?text=#{CGI.escape(quiz.title)}"
+    # 画像のテキストは、クイズのタイトル、問題数、タグを使う
+    image_url = "#{request.base_url}/images/ogp.png?text=#{CGI.escape(quiz.title)}&questions_count=#{quiz.questions_count}&tags=#{CGI.escape(quiz.tags.pluck(:name).join(','))}"
     set_meta_tags og: {
                       site_name: 'Programming Question',
-                      title: "テストタイトル",
+                      title: quiz.title,
                       description: 'Programming Questionはプログラミングのクイズを投稿・共有できるサイトです。',
                       type: 'website',
                       url: request.original_url,
