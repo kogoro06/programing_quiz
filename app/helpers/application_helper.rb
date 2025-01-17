f56661f56bc08ee59a8ae1020f1b8fbdb3a11134
taka4655 <taku.enginner@gmail.com>
2025-01-17 16:05:56 +0900

    fix:Lintチェックエラー解消

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
                       image: image_url
                   }
   end
-  
+
   def page_title(title = "")
     base_title = "Programming Question"
     title.present? ? "#{title} | #{base_title}" : base_title
