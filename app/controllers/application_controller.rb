05c7f4e362e7fdffe943a39ae823d1a72c78c0db
taka4655 <taku.enginner@gmail.com>
2025-01-20 16:57:29 +0900

    動的ogp修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,25 +7,30 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def prepare_meta_tags(quiz)
-    # 画像のテキストは、クイズのタイトル、問題数、タグを使う
-    image_url = "#{request.base_url}/images/ogp.png?text=#{CGI.escape(quiz.title)}&questions_count=#{quiz.questions_count}&tags=#{CGI.escape(quiz.tags.pluck(:name).join(','))}"
-    quiz_url = url_for(controller: "quiz_posts", action: "show", id: quiz.id, only_path: false)
-    set_meta_tags og: {
-                      site_name: "Programming Question",
-                      title: quiz.title,
-                      description: "Programming Questionはプログラミングのクイズを投稿・共有できるサイトです。",
-                      type: "website",
-                      url: quiz_url,
-                      image: image_url,
-                      local: "ja-JP"
-                  },
-                  twitter: {
-                      card: "summary_large_image",
-                      site: "@study_kogoro", # 公式アカウントのユーザー名を入れる
-                      title: quiz.title,
-                      description: "Programming Questionはプログラミングのクイズを投稿・共有できるサイトです。",
-                      image: image_url
-                  }
+    image_url = "#{request.base_url}/images/ogp.png?text=#{CGI.escape(quiz.title)}&questions_count=#{quiz.questions_count}&tags=#{CGI.escape(quiz.tags.pluck(:name).join(','))}",
+    quiz_url = url_for(controller: "quiz_posts", action: "show", id: quiz.id, only_path: false),
+
+    set_meta_tags(
+      title: quiz.title,
+      description: "Programming Questionはプログラミングのクイズを投稿・共有できるサイトです。",
+      # 画像のテキストは、クイズのタイトル、問題数、タグを使う
+      og: {
+          site_name: "Programming Question",
+          title: quiz.title,
+          description: "Programming Questionはプログラミングのクイズを投稿・共有できるサイトです。",
+          type: "website",
+          url: quiz_url,
+          image: image_url,
+          local: "ja-JP"
+      },
+      twitter: {
+          card: "summary_large_image",
+          site: "@study_kogoro", # 公式アカウントのユーザー名を入れる
+          title: quiz.title,
+          description: "Programming Questionはプログラミングのクイズを投稿・共有できるサイトです。",
+          image: image_url
+      }
+    )
   end
 
 
