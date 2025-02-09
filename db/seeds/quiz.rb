dc7153153720b43f8a003c5cfae46ede52fa66fa
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-09 14:01:39 +0900

    feat: Quiz, Questions作成ロジックを記述

--- db/seeds/quiz.rb
+++ db/seeds/quiz.rb
@@ -1,3 +1,19 @@
 require "csv"
 
 puts "\n=== Creating Quizzes ==="
+CSV.foreach("db/csv/Separated_Quizzes.csv", headers: true) do |row|
+  quiz = Quiz.create!(title: row["quiz_title"]) do |q|
+    q.author_user_id = row["author_user_id"]
+    q.title = row["quiz_title"]
+    q.questions_count = 1
+    q.questions.build(
+      question: row["question"],
+      correct_answer: row["correct_answer"],
+      answer_source: row["answer_source"],
+      explanation: row["explanation"]
+    )
+    p q.questions
+    q.save(validate: false)
+  end
+  print "."
+end
