a4646ca71da97137e5df01c81963cad4791bacf6
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-02-20 15:14:15 +0900

    モバイルでも機能させるため、allow_browserの記載を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
   before_action :set_user
   before_action :set_profile
   before_action :set_search
