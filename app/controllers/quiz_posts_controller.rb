a6e36be5160b2495bcc79d4ee1f6e453f767a7e9
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-09 11:39:36 +0900

    fix: 部分更新を見越した、save成功時の処理変更

--- app/controllers/quiz_posts_controller.rb
+++ app/controllers/quiz_posts_controller.rb
@@ -37,21 +37,23 @@ class QuizPostsController < ApplicationController
 
     @quiz.questions = @quiz.questions.select { |q| q.question.present? }
 
-    if @quiz.save
-      redirect_to quiz_post_path(@quiz), notice: "クイズを投稿しました！"
-    else
-      if current_user.admin?
-        30.times do
-          question = @quiz.questions.build
-          question.choices.build # 1回だけchoicesを作成
-        end
+    respond_to do |format|
+      if @quiz.save
+        format.html { redirect_to quiz_post_path(@quiz), notice: "クイズを投稿しました！" }
       else
-        10.times do
-          question = @quiz.questions.build
-          question.choices.build # 1回だけchoicesを作成
+        if current_user.admin?
+          30.times do
+            question = @quiz.questions.build
+            question.choices.build # 1回だけchoicesを作成
+          end
+        else
+          10.times do
+            question = @quiz.questions.build
+            question.choices.build # 1回だけchoicesを作成
+          end
         end
+        render :new, status: :unprocessable_entity
       end
-      render :new, status: :unprocessable_entity
     end
     Rails.logger.info "Received tag_ids: #{params[:quiz][:tag_ids]}"
   end
