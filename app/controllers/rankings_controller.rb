7af8b1d26935b3e58186db194ad513c1c84386ce
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-23 23:11:35 +0900

    fix: インスタンス変数名変更

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,6 +1,6 @@
 class RankingsController < ApplicationController
   def index
     @most_solved_quizzes = get_most_solved_quizzes
-    @engaged_users = set_engaged_users
+    @most_engaged_users = set_engaged_users
   end
 end
